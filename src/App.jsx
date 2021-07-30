3d6dbd5466114fb878d6df1e81a4587b351f5965
puneetsingh99 <99singhpuneet99@gmail.com>
2021-07-30 15:30:31 +0530

    refactor: css minifiy

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="App bg-green-300">
-      <p className="text-3xl text-red-500"> vite reactjs app</p>
+    <div className="App">
+      <p className="text-3xl"> vite reactjs app</p>
     </div>
   );
 }
